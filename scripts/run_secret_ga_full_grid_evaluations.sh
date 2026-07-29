26a9f70912f0c6a852e008f94f29dc2a4af56976
Dogukan <dbaysal2001@gmail.com>
2026-07-29 16:06:45 +0300

    script fix

--- scripts/run_secret_ga_full_grid_evaluations.sh
+++ scripts/run_secret_ga_full_grid_evaluations.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 exec env \
     RUN_UNLEARNING=0 \
     RUN_EVALUATIONS=1 \
+    EVAL_MODEL_SOURCE=hub \
     bash "${SCRIPT_DIR}/run_secret_ga_full_grid_search.sh" "$@"
